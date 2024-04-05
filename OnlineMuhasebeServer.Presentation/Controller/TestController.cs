28b79817db298bad301e94a8e467c9563db9c47c
Selim BAYINDIR <byndr28@gmail.com>
2024-04-05 22:30:48 +0300

    Add project files.

---
+++ OnlineMuhasebeServer.Presentation/Controller/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineMuhasebeServer.Presentation.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMuhasebeServer.Presentation.Controller
+{
+    public sealed class TestController :ApiController
+        //sealed inherit edilmesini engeller
+    {
+        [HttpGet]
+        public IActionResult GET()
+        {
+            {
+                return Ok("işlem Başarılı");
+            }
+        }
+    }
+}
