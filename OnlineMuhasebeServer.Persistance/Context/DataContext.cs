577b1102a3df4d79a1c821d958e806da9512376a
Selim BAYINDIR <byndr28@gmail.com>
2024-04-05 23:49:40 +0300

    ıdentity Claim

---
+++ OnlineMuhasebeServer.Persistance/Context/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMuhasebeServer.Persistance.Context
+{
+    public class DataContext : IdentityDbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
